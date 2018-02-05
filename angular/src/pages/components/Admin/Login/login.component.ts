f63256dedf560c9bd0ee06c804625b9a0a176415
Sujan Shrestha <sujan_sth101@outlook.com>
2018-02-05 23:19:45 +0545

    login completed

--- angular/src/pages/components/Admin/Login/login.component.ts
+++ angular/src/pages/components/Admin/Login/login.component.ts
@@ -4,24 +4,30 @@ import { AuthenticationService, MenuService } from '../../../../services/base.se
 import { DomSanitizer } from '@angular/platform-browser';
 
 @Component({
-    moduleId: module.id,
-    selector: 'app-admin',
-    templateUrl: './login.component.html',
-    providers: [AuthenticationService],
+  moduleId: module.id,
+  selector: 'app-admin',
+  templateUrl: './login.component.html',
+  providers: [AuthenticationService],
 })
 export class LoginComponent {
   loginForm: IAuthentication = <IAuthentication>{};
 
   errMessage: string;
+  isLoading: boolean;
 
   constructor(
     public authenticationService: AuthenticationService
-  ) {}
+  ) { }
 
   submitLoginForm() {
+    this.isLoading = true;
     this.authenticationService.login(this.loginForm.UserName, this.loginForm.Password).subscribe((data: IUser) => {
+      this.isLoading = false;
       localStorage.setItem('UserInfo', JSON.stringify(data));
+      this.errMessage = null;
+      window.location.reload();
     }, (err) => {
+      this.isLoading = false;
       this.errMessage = 'Login failed';
     });
   }
