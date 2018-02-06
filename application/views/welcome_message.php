576aa80e575b5f44c457a85f052aaa6611bcf4de
Utsav Shakya <utsavshakya@hotmail.com>
2018-02-06 15:13:47 +0545

    title

--- application/views/welcome_message.php
+++ application/views/welcome_message.php
@@ -6,7 +6,7 @@ defined('BASEPATH') or exit('No direct script access allowed');
 <html lang="en">
 <head>
   <meta charset="utf-8">
-  <title>Angular</title>
+  <title>Venus IVF Center</title>
   <base href="/sumeet/">
 
   <meta name="viewport" content="width=device-width, initial-scale=1">
